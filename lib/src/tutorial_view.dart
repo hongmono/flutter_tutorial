930c3ea0b51f1d1c766f4e614bbd1acadd03fdfa
hanbit <67232509+wolru@users.noreply.github.com>
2024-01-16 18:58:20 +0900

    EGG-2207 pop 전에 controller dispose

--- lib/src/tutorial_view.dart
+++ lib/src/tutorial_view.dart
@@ -48,9 +48,8 @@ class _TutorialViewState extends State<TutorialView> {
   @override
   Widget build(BuildContext context) {
     if (messages.isEmpty) {
-      WidgetsBinding.instance.addPostFrameCallback((_) {
-        if (mounted) Navigator.pop(context);
-      });
+      controller.dispose();
+      Navigator.pop(context);
       return const SizedBox.shrink();
     }
 
